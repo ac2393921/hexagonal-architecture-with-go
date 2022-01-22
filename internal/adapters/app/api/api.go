e1e3830d697548a6e639192911ea6a3702c62e50
ac2393921 <ac2393921@gmail.com>
2022-01-23 00:55:51 +0900

    add: create connect db and insert history func.

--- internal/adapters/app/api/api.go
+++ internal/adapters/app/api/api.go
@@ -5,15 +5,21 @@ import (
 )
 
 type Adapter struct {
+	db    ports.DbPort
 	arith ports.ArithmeticPort
 }
 
-func NewAdapter(arith ports.ArithmeticPort) *Adapter {
-	return &Adapter{arith: arith}
+func NewAdapter(db ports.DbPort, arith ports.ArithmeticPort) *Adapter {
+	return &Adapter{db: db, arith: arith}
 }
 
 func (apia Adapter) GetAddition(a, b int32) (int32, error) {
 	answer, err := apia.arith.Addition(a, b)
+	if err != nil {
+		return 0, err
+	}
+
+	err = apia.db.AddToHistory(answer, "addition")
 	if err != nil {
 		return 0, err
 	}
@@ -27,6 +33,11 @@ func (apia Adapter) GetSubtraction(a, b int32) (int32, error) {
 		return 0, err
 	}
 
+	err = apia.db.AddToHistory(answer, "substraction")
+	if err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
@@ -36,11 +47,21 @@ func (apia Adapter) GetMultiplication(a, b int32) (int32, error) {
 		return 0, err
 	}
 
+	err = apia.db.AddToHistory(answer, "multiplication")
+	if err != nil {
+		return 0, err
+	}
+
 	return answer, nil
 }
 
 func (apia Adapter) GetDivision(a, b int32) (int32, error) {
 	answer, err := apia.arith.Division(a, b)
+	if err != nil {
+		return 0, err
+	}
+
+	err = apia.db.AddToHistory(answer, "division")
 	if err != nil {
 		return 0, err
 	}
