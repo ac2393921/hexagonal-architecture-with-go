593057b7794a906285185c75980cad4faba814cf
ac2393921 <ac2393921@gmail.com>
2022-01-23 19:17:23 +0900

    update: write main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,34 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/ac2393921/hexagonal-architecture-with-go/internal/adapters/app/api"
+	gRPC "github.com/ac2393921/hexagonal-architecture-with-go/internal/adapters/framework/left/grpc"
+	"github.com/ac2393921/hexagonal-architecture-with-go/internal/adapters/framework/right/db"
+	"github.com/ac2393921/hexagonal-architecture-with-go/internal/ports"
+)
+
 func main() {
-	// var core ports.ArithmeticPort
-	// core = arithmetic.NewAdapter()
+	var err error
+
+	var dbaseAdapter ports.DbPort
+	var core ports.ArithmeticPort
+	var appAdapter ports.APIPort
+	var gRPCAdapter ports.GRPCPort
+
+	dbaseDriver := os.Getenv("DB_DRIVER")
+	dsourceName := os.Getenv("DS_NAME")
+
+	dbaseAdapter, err = db.NewAdapter(dbaseDriver, dsourceName)
+	if err != nil {
+		log.Fatalf("failed to initiate dbase connection: %v", err)
+	}
+	defer dbaseAdapter.CloseDbConnection()
+
+	appAdapter = api.NewAdapter(dbaseAdapter, core)
+
+	gRPCAdapter = gRPC.NewAdapter(appAdapter)
+	gRPCAdapter.Run()
 }
